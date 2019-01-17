09446c1e5176268c48b10111d657581a16be771b
zhangzhen <1094705507@qq.com>
2019-01-18 00:18:11 +0800

    test

---
+++ test/index.ts
@@ -0,0 +1,4 @@
+import Hello from "./test"
+let hello: Hello = new Hello();
+console.log(hello.hello);
+console.log(hello.world)
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+interface test1{
+    hello:string,
+    world:string
+}
+export default class Hello implements test1{
+    hello:string = "zhangzhen";
+    world:string = "hongyannan";
+}
